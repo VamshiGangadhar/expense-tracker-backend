f73afe70eb6a895f96e48c6388b8585d849013e9
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2024-12-30 15:01:03 +0530

    added mongo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const userRoutes = require('./routes/userRoutes');
-const testRoutes = require('./routes/testRoutes');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
+const testRoutes = require("./routes/testRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/test', testRoutes);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/api/users", userRoutes);
+app.use("/api/test", testRoutes);
 
 app.listen(3001, () => {
   console.log("Server running on port 3000");
